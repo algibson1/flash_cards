77f9c72c0922a85b3c5c01cec00f4bd209477321
Artemy Gibson <artemygibson@gmail.com>
2023-05-08 18:10:59 -0700

    Add test for negative feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -23,4 +23,13 @@ RSpec.describe Turn do
         expect(turn.correct?).to be true
         expect(turn.feedback).to eq("Correct!")
     end
+
+    it 'gives feedback on negative answers' do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+        expect(turn.card).to eq(card)
+        expect(turn.guess).to eq("Saturn")
+        expect(turn.correct?).to be false
+        expect(turn.feedback).to eq("Incorrect.")
+    end
 end
