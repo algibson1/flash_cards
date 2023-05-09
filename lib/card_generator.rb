f1ebf79a0f5f0d44c283e473a6c8231784e766bc
Artemy Gibson <artemygibson@gmail.com>
2023-05-09 16:34:26 -0700

    Pass test to generate cards from txt file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,14 +1,15 @@
 class CardGenerator
     attr_reader :cards
     def initialize(filename)
-        @cards = []
+        @cards = generate(filename)
     end
-    # def generate(filename)
-    #     cards = []
-    #     file.open(filename).each do |line|
-    #         line = line.to_s
-    #         new_card = /(?<question>\w+)\,(?<answer>\w+)\,(?<category>\w+)/
-    #         require 'pry'; binding.pry
-    #     end
-    # end
+    def generate(filename)
+        cards = []
+        File.open("./lib/#{filename}").each do |line|
+            line = line.to_s
+            new_card = line.match /(?<question>.+)\,(?<answer>.+)\,(?<category>.+)/
+            cards.push(Card.new(new_card[:question], new_card[:answer], new_card[:category]))
+        end
+        cards
+    end
 end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,11 +1,17 @@
-# require "./lib/card"
+require "./lib/card"
 require "./lib/card_generator"
-# require "./lib/cards.txt"
 
 RSpec.describe CardGenerator do
     it 'exists' do
         filename = "cards.txt"
         cards = CardGenerator.new(filename)
         expect(cards).to be_an_instance_of(CardGenerator)
+    end
+
+    it 'creates cards from file' do
+        card1 = Card.new("What is the capital of Delaware?","Dover",:Geography)
+        filename = "cards.txt"
+        cards = CardGenerator.new(filename).cards
+        expect(cards.first.answer).to eq(card1.answer)
     end
 end
